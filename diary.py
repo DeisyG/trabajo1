07e32b1614e8787126559261a5d943270faf47ac
daya <daya_932@hotmail.com>
2018-05-21 10:48:29 -0500

    commit 'deber1'

--- diary.py
+++ diary.py
@@ -1,65 +1,98 @@
-import sys
 from collections import OrderedDict
 import datetime
+import sys
+import os
 from peewee import *
 
-
-db=SqliteDatabase('diary.db')
+db= SqliteDatabase('diary.db')
 
 class Entry(Model):
+
     content=TextField()
-    timestamp=DateTimeField(default=datetime.datetime.now)#coger la fecha y hora
+    timestamp=DateTimeField(default=datetime.datetime.now)
+
     class Meta:
-        database=db
+        database = db
+
 def add_entry():
-    """Agregar un registro"""
-    print("Introduzca un registro, Presiones Ctrl+z+Enter para terminar")
-    data=sys.stdin.read().strip()
+    """Agrega un registro"""
+    print("Introduce tu registro, Presione Ctrl+Z+Enter cuando termines ")
+
+    data = sys.stdin.read().strip()
     if data:
-        if input("Desea guardar este registro [Y/N]: ").lower() !='y':
+        if input('Guardar entrada? [Y/N]: ').lower() != 'n': #si presiona cualquier tecla diferente a n guarda
             Entry.create(content=data)
-            print("Se ha guardado exitosamente")
+            print("GUARDADA EXITOSAMENTE")
 
-def view_entries():
-    """Ver un registro"""
 
+def view_entries(search_query=None):
+    """Ver nuestras entras"""
     entries= Entry.select().order_by(Entry.timestamp.desc())
+
+    if search_query:
+        entries= entries.where(Entry.content.contains(search_query))
     for entry in entries:
-        timestamp= entry.timestamp.strftime('%A %B %d, %Y %I:%M%p')
+
+        timestamp= entry.timestamp.strftime('%A %B %d, %Y %I:%M%p') #formato que deseen
+        clear()
         print(timestamp)
-        print("*******************")
+        print('+'*len(timestamp))
+        #print de esta entrada
         print(entry.content)
-        print("n---> Ver el siguiente registro")
-        print("q---> Salir del menu")
+        print('\n\n\n'+'+'*len(timestamp)+'\n')
+        print("n-->siguiente entrada")
+        print("q-->salir del menu")
+        print("d-->borrar entrada")
 
-        next_entry=input("Desea ver la siguiente entrada [n/q]: ").lower().strip()
-        if next_entry == 'q':
+        next_action=input('Accion a realizar [n/q/d]: ').lower().strip()
+        if next_action == 'q':
             break
-def delete_entries():
-    """Eliminar un registro"""
+        elif next_action == 'd':
+            delete_entry(entry)
 
-menu=OrderedDict([
-    ('a',add_entry),
-    ('v',view_entries),
+def search_entries():
+    """Busca una entrada con cierto texto"""
 
+    view_entries(input('Texto a buscar: '))
+
+def delete_entry(entry):
+    """Borra un registro"""
+
+    response=input("Estas seguro? [Y/N]: ").lower()
+    if response == 'y':
+        entry.delete_instance()
+        print("ENTRADA BORRADA")
+
+menu = OrderedDict([
+    ('a', add_entry),
+    ('v', view_entries),
+    ('s', search_entries),
 ])
 
 def menu_loop():
-    """Mostrar opciones"""
-    choice=None
-    while choice != 'x':
-        print("Presiona 'x' para salir")
+    """Muestra el menu con las opciones"""
 
-        for key, value in menu.items():#nos permite corre cada uno de lo elementos que tenemos en los corchetes
-            print('{}--->{}'.format(key, value.__doc__))
-        choice=input("Escoja opcion: ").lower().strip()#strip permite eliminar espacio o caracteres
+    choice= None
+
+    while choice != 'q':
+        clear()
+        print("Presiona 'q' para salir")
+
+        for key, value in menu.items():
+            print('{}| {}'.format(key, value.__doc__))
+        choice= input('Eleccion: ').lower().strip()
         if choice in menu:
+            clear()
             menu[choice]()
 
-def init():
+def initialize():
     db.connect()
     db.create_tables([Entry],safe=True)
 
+def clear():
+    os.system('cls' if os.name== 'nt' else 'clear')
+
+
 if __name__ == '__main__':
-    init()
+    initialize()
     menu_loop()
